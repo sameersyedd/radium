8c15a078218afe0620fbeefb9c6caabc77ca6679
sameersyedd <smsameer.sameer@gmail.com>
2021-11-15 01:10:29 +0530

    MongoDB and Scheme Basics

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
 const router = express.Router();
+const BookModel = require("../models/bookModel")
 
-router.get('/test-me', function (req, res) {
+const BookController = require("../controllers/bookController")
+
+
+router.get('/test-me', function(req, res) {
     res.send('My first ever api!')
 });
+
+router.post('/createBook', BookController.createBook);
+router.get('/getAllBook', BookController.getBookData);
+
 
 module.exports = router;
